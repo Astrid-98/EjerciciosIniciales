0c27d308d119fd53b0b4e06da3f2e1e751045b7a
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-09-15 16:18:21 +0200

    Terminado

--- Assets/EjerciciosIniciales/Ejercicio1_1.cs
+++ Assets/EjerciciosIniciales/Ejercicio1_1.cs
@@ -7,12 +7,17 @@ public class Ejercicio1_1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hola Mundo!");
+        Debug.Log(“Este es el primer videojuego de Leslie Portilla”);
+        Debug.Log(“Estoy aprendiendo C#”);
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(“Ha pasado un frame”);
     }
 }
---
+++ Assets/EjerciciosIniciales/Ejercicio2_1.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_1 : MonoBehaviour
+{
+    int edad = 26;
+    float altura = 1.56f;
+    string nombre = Astrid;
+    int iva =;
+    float peso = 56.4;
+    char letra = 'A';
+    int minutos = 30;
+    string matriculaCoche = "ASD04FGH";
+    bool mayorDeEdad = true;
+    int codigoPortal = 28045;
+    bool genero = mujer;
+    int numeroDeHijos = 0;
+    int tallaCamisa = 36;
+    float precio = 22.51f;
+    string mensaje = "otro día";
+    bool mayorEdad = true;
+    int dias = 5;
+    int contador;
+    string tallaCamiseta = "S";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
