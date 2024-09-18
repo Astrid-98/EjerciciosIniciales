c14bc866186c0e32a35b3201ef5c5c029644d866
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-09-18 17:51:14 +0200

    fin

--- Assets/EjerciciosIniciales/Ejercicios7_3.cs
+++ Assets/EjerciciosIniciales/Ejercicios7_3.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Ejercicios7_3 : MonoBehaviour
 {
+    int vidas = 100;
     // Start is called before the first frame update
     void Start()
     {
+        while (vidas < 0)
+        {
+            Debug.Log("Sigo vivo" + vidas);
+            vidas--;
+        }
         
     }
 
