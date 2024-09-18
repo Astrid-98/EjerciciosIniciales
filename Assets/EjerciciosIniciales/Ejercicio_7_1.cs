5c9451b35afcd232228d317ff322d272ae8a2f8f
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-09-18 16:52:21 +0200

    Hecho

--- Assets/EjerciciosIniciales/Ejercicio_7_1.cs
+++ Assets/EjerciciosIniciales/Ejercicio_7_1.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ejercicio2_8 : MonoBehaviour
+public class Ejercicio_7_1 : MonoBehaviour
 {
-    int vidas = 100;
+    int vidas = 1;
     // Start is called before the first frame update
     void Start()
     {
-        while (vidas > 0)
+        while ( vidas < 100)
         {
-            Debug.Log("Sigo vivo");
-            vidas--;
+            Debug.Log("Sigo vivo" + vidas);
+            vidas++;
         }
-
     }
 
     // Update is called once per frame
--- Assets/EjerciciosIniciales/Ejercicios7_3.cs
+++ Assets/EjerciciosIniciales/Ejercicios7_3.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ejercicio2_8 : MonoBehaviour
+public class Ejercicios7_3 : MonoBehaviour
 {
-    int vidas = 100;
     // Start is called before the first frame update
     void Start()
     {
-        while (vidas > 0)
-        {
-            Debug.Log("Sigo vivo");
-            vidas--;
-        }
-
+        
     }
 
     // Update is called once per frame
