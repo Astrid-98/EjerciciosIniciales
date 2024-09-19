68a2368a19d2f6ed14526a946a0a3aba51b3fb2d
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-09-19 13:39:03 +0200

    hecho

--- Assets/EjerciciosIniciales/Ejercicio3_7.cs
+++ Assets/EjerciciosIniciales/Ejercicio3_7.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Ejercicio3_7 : MonoBehaviour
 {
+    float baseTriangulo = 14f;
+    float alturaTriangulo = 17.5f;
+
+    float area;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        area = (baseTriangulo * alturaTriangulo) / 2;
+        Debug.Log(area);
     }
 
     // Update is called once per frame
