3cf379891b06116d20aa8367a0bcacb0ba4dda82
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-09-19 23:27:06 +0200

    creo que esta bien

--- Assets/EjerciciosIniciales/Ejercicio2_8.cs
+++ Assets/EjerciciosIniciales/Ejercicio2_8.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class Ejercicio2_8 : MonoBehaviour
 {
-    float vidaInicial = 120;
-    float porcentaje = 0.03f;
-    float vida;
+    float vidaInicial = 120f;
+    
+    float turno1;
+    float turno2;
+    float turno3;
+    float turno4;
+    float turno5;
 
 
 
     
     void Start()
     {
-        //vida = vidaInicial;
-        //Debug.Log(vida);
+        turno1 = 3 * vidaInicial / 100;
+        Debug.Log(turno1);
+
+        turno2 = 6 * vidaInicial / 100;
+        Debug.Log(turno2);
+
+        turno3 = 9 * vidaInicial / 100;
+        Debug.Log(turno3);
+
+        turno4 = 12 * vidaInicial / 100;
+        Debug.Log(turno4);
+
+        turno5 = 15 * vidaInicial / 100;
+        Debug.Log(turno5);
+           
     }
     // Update is called once per frame
     void Update()
