950199ef02645b42aa5c91c8a74c38f5ea9720d3
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-09-19 13:30:12 +0200

    creo que esta bien

--- Assets/EjerciciosIniciales/Ejercicio3_5.cs
+++ Assets/EjerciciosIniciales/Ejercicio3_5.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Ejercicio3_5 : MonoBehaviour
 {
+    float radioCircunferencia = 83f;
+    float pi = 3.14159265358979f;
+
+    float longitudCircunferencia;
+    float areaCircunferencia;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        longitudCircunferencia = 2 * pi * radioCircunferencia;
+        areaCircunferencia = (pi * radioCircunferencia) * (pi * radioCircunferencia);
+        Debug.Log(longitudCircunferencia);
+        Debug.Log(areaCircunferencia);
+
     }
 
     // Update is called once per frame
