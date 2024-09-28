31baebe519b4f44b754c705abd827c0de7022226
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-09-28 23:11:29 +0200

    nop

--- Assets/EjerciciosIniciales/Ejercicio5/Ejercicio5_8.cs
+++ Assets/EjerciciosIniciales/Ejercicio5/Ejercicio5_8.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Ejercicio5_8 : MonoBehaviour
 {
+   [SerializeField] int horas; // de 0 a 23
+   [SerializeField] int minutos; // de 0 a 59
+   [SerializeField] int segundos; // de 0 a 59
+
+    int miHora;
+
+    // >= <=
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+       if(horas >= 0 && horas <= 23 || minutos >= 0 && minutos <= 59 || segundos >= 0 && segundos <= 59)
+       {
+            Debug.Log(horas + " : " + minutos + " : " + segundos);
+       }
+       else
+       {
+            Debug.Log("Error");
+       }
     }
 }
