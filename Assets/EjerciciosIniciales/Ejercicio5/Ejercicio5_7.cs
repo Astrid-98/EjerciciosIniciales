6c984bb68dcf665901f6b89e17ca2a0dd4633856
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-09-28 22:45:33 +0200

    terminado

--- Assets/EjerciciosIniciales/Ejercicio5/Ejercicio5_7.cs
+++ Assets/EjerciciosIniciales/Ejercicio5/Ejercicio5_7.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Ejercicio5_7 : MonoBehaviour
 {
+    int velocidadJugador1 = 25;  
+    int velocidadJugador2 = 10;
+    int velocidadJugador3 = 75;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (velocidadJugador1 > velocidadJugador2 && velocidadJugador1 > velocidadJugador3) 
+        {
+            Debug.Log("Jugador 1 primero");
+        } 
+        else if (velocidadJugador2 > velocidadJugador1 && velocidadJugador2 > velocidadJugador3) 
+        {
+        Debug.Log("Jugador 2 primero");
+        }
+        else if (velocidadJugador3 > velocidadJugador1 && velocidadJugador3 > velocidadJugador2)
+        {
+            Debug.Log("Jugador 3 primero");
+        }
     }
 
     // Update is called once per frame
