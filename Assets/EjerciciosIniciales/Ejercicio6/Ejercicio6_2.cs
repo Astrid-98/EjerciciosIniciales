b29c198bbaeea0b978acd5b29b35b750653e78ec
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-09-29 01:17:06 +0200

    hecho

---
+++ Assets/EjerciciosIniciales/Ejercicio6/Ejercicio6_2.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6_2 : MonoBehaviour
+{
+    // a. F
+    // b. T
+    // c. T
+    // d. T
+    // e. T
+    // f. F
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
