b79f6b5afcc6f54ad5cfa783c63c6df3174a260c
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-09-29 00:01:41 +0200

    terminado

--- Assets/EjerciciosIniciales/Ejercicio5/Ejercicio5_9.cs
+++ Assets/EjerciciosIniciales/Ejercicio5/Ejercicio5_9.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Ejercicio5_9 : MonoBehaviour
 {
+    [SerializeField] int enemigoTipo;
+  
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if( enemigoTipo == 1)
+        {
+            int danho = 350;
+            int vida = 650;
+            Debug.Log("Daño " + danho + " y vida " + vida);
+        }
+        else if( enemigoTipo == 2) 
+        {
+            int danho = 300;
+            int vida = 550;
+            Debug.Log("Daño " + danho + " y vida " + vida);
+        }
+        else if (enemigoTipo == 3)
+        {
+            int danho = 300;
+            int vida = 500;
+            Debug.Log("Daño " + danho + " y vida " + vida);
+        }
+        else if (enemigoTipo == 4)
+        {
+            int danho = 310;
+            int vida = 460;
+            Debug.Log("Daño " + danho + " y vida " + vida);
+        }
+        else if (enemigoTipo == 5)
+        {
+            int danho = 280;
+            int vida = 490;
+            Debug.Log("Daño " + danho + " y vida " + vida);
+        }
+        else if (enemigoTipo == 6)
+        {
+            int danho = 360;
+            int vida = 520;
+            Debug.Log("Daño " + danho + " y vida " + vida);
+        }
     }
 }
