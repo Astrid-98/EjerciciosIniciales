24344bdce733f5db4b3786289360baddd30cc637
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-09-29 15:00:29 +0200

    corregido

--- Assets/EjerciciosIniciales/Ejercicio7/Ejercicio7_5.cs
+++ Assets/EjerciciosIniciales/Ejercicio7/Ejercicio7_5.cs
@@ -2,18 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ejercicios7_3 : MonoBehaviour
+public class Ejercicio7_5 : MonoBehaviour
 {
-    int vidas = 100;
-
     // Start is called before the first frame update
     void Start()
     {
-        while (vidas > 0)
-        {
-            Debug.Log(vidas);
-            vidas--;
-        }
         
     }
 
