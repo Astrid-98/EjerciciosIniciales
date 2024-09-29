3b2f41ea0e97d09a10ce4f7dbd657541a7071ddb
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-09-29 15:26:39 +0200

    este me ha costado entenderlo

--- Assets/EjerciciosIniciales/Ejercicio7/Ejercicio7_5.cs
+++ Assets/EjerciciosIniciales/Ejercicio7/Ejercicio7_5.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Ejercicio7_5 : MonoBehaviour
 {
+    [SerializeField] int numero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int miNumero = 1;
+        while (miNumero <= numero) //mientras miNumero 1 sea menor o igual a el numero que yo ponga en unity
+        {
+            Debug.Log(miNumero);  //imprime miNumero
+            miNumero++;           //de uno en uno
+        }
     }
 
     // Update is called once per frame
