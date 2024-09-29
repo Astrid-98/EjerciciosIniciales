6820f06b05b31bc3cea673a856e88e89736c2eb5
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-09-29 15:04:19 +0200

    hecho

--- Assets/EjerciciosIniciales/Ejercicio7/Ejercicio7_4.cs
+++ Assets/EjerciciosIniciales/Ejercicio7/Ejercicio7_4.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Ejercicio7_4 : MonoBehaviour
 {
+    int numero;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   //desde numero 100, hata 1, de uno en uno.
+        for (int numero = 100; numero > 1; numero--)
+        {
+            Debug.Log(numero);
+        }
     }
 
     // Update is called once per frame
